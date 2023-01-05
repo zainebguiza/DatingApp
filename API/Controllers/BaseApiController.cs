4bde78707b1591752c400365bf8eed0e90d2e32a
Zaineb <zainebguiza99@outlook.fr>
2023-01-05 14:53:42 +0100

    End of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{   [ApiController]
+    [Route("api/[controller]")] 
+    public class BaseApiController:ControllerBase
+    {
+
+    }
+}
