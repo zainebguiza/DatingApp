e6eddc05e90dc5239423610a8946898b97cf891d
Zaineb <zainebguiza99@outlook.fr>
2023-02-13 11:52:21 +0100

    End of section 15

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from 'ngx-gallery-9';
 import { Member } from 'src/app/_models/member';
+import { Message } from 'src/app/_models/message';
 import { MembersService } from 'src/app/_services/members.service';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -10,14 +13,25 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  member:Member |undefined
+  @ViewChild('memberTabs' ,{static:true}) memberTabs?:TabsetComponent;
+  member:Member ={} as Member;
   galleryOptions: NgxGalleryOptions[] = [];
   galleryImages: NgxGalleryImage[] = [];
+  activeTab?:TabDirective;
+  messages:Message[]=[];
   
-  constructor(private memberService:MembersService,private route:ActivatedRoute) { }
+  constructor(private memberService:MembersService,private route:ActivatedRoute
+    ,private messageService:MessageService) { }
 
   ngOnInit(): void {
-    this.loadMember();
+    this.route.data.subscribe({
+      next:data=>this.member=data['member']
+    })
+    this.route.queryParams.subscribe({
+      next:params=>{
+        params['tab']&& this.selectTab(params['tab'])
+      }
+    })
     this.galleryOptions=[{
       width:'500px',
       height:'500px',
@@ -27,7 +41,7 @@ export class MemberDetailComponent implements OnInit {
       preview: false
     }
   ]
-
+  this.galleryImages=this.getImages();
   }
   
   getImages() {
@@ -42,16 +56,26 @@ export class MemberDetailComponent implements OnInit {
     }
     return imageUrls;
   }
+  
+  selectTab(heading:string){
+     if(this.memberTabs){
+      this.memberTabs.tabs.find(x=>x.heading===heading)!.active=true
+     }
+  }
 
-  loadMember(){
-    var username=this.route.snapshot.paramMap.get('username')
-    if(!username)return;
-    this.memberService.getMember(username).subscribe({
-      next:member=>{ 
-        this.member=member;
-        this.galleryImages=this.getImages();
-      }
-    })
+  loadMessages(){
+    if(this.member){
+      this.messageService.getMessageThread(this.member.userName).subscribe({
+        next:messages=>this.messages=messages
+      })
+    }
+  }
+
+  onTabActivated(data: TabDirective){
+    this.activeTab=data;
+    if(this.activeTab.heading==='Messages'){
+      this.loadMessages();
+    }
   }
 
 }
